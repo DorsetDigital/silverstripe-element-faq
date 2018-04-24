b7eebb1177e410b0c5f96141cf1885835f505c8a
Tim Burt <tim@dorset-digital.net>
2018-04-24 20:28:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# silverstripe-element-iframe
+Adds an elemental content block to embed an iframe
+
+[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](LICENSE.md)
+
+# Requirements
+*Silverstripe 4.x
+*Silverstripe Elemental
+
+# Installation
+* Install the code with `composer require dorsetdigital/silverstripe-element-iframe`
+* Run a `dev/build?flush` to update your project
+
+# Usage
+This module adds a simple way to embed an iframe into your Elemental page
+By default, the module doesn't add any styling, it expects you to do this in your own CSS file
---
+++ src/IframeElement.php
@@ -0,0 +1,30 @@
+<?php
+namespace DorsetDigital\SilverStripeElementIframe;
+
+use DNADesign\Elemental\Models\BaseElement;
+use SilverStripe\Forms\TextField;
+
+
+class IframeElement extends BaseElement
+{
+
+    private static $singular_name = 'Iframe Block';
+    private static $plural_name = 'Iframe Blocks';
+    private static $description = 'Embeds an Iframe into the page';
+    private static $table_name = 'DorsetDigital_Elements_Iframe';
+    private static $db = [        
+        'EmbedLink' => 'Varchar(1024)'
+    ];
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->addFieldToTab('Root.Main', TextField::create('EmbedLink')->setTitle('Iframe link')->setDescription('The full Iframe code as supplied by the site to be embedded.'));
+        return $fields;
+    }
+
+    public function getType()
+    {
+        return 'Iframe block';
+    }
+}

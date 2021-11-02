c76970edf706366cffeb690707d816763a6a14d1
Tim Burt <tim@dorset-digital.net>
2021-11-02 09:08:44 +0000

    Make permission translatable Code tidy

--- src/FAQ.php
+++ src/FAQ.php
@@ -3,10 +3,10 @@
 namespace DorsetDigital\Elements;
 
 use SilverStripe\ORM\DataObject;
-use SilverStripe\Security\PermissionProvider;
-use SilverStripe\Security\Permission;
-use SilverStripe\Security\Security;
 use SilverStripe\Security\Member;
+use SilverStripe\Security\Permission;
+use SilverStripe\Security\PermissionProvider;
+use SilverStripe\Security\Security;
 
 class FAQ extends DataObject implements PermissionProvider
 {
@@ -24,26 +24,30 @@ class FAQ extends DataObject implements PermissionProvider
     private static $summary_fields = [
         'Question'
     ];
-    
-    public function providePermissions() {
-      return [
-        'CAN_ACCESS_FAQS' => 'View create and edit FAQs in FAQ block',
-      ];
+
+    public function providePermissions()
+    {
+        return [
+            'CAN_ACCESS_FAQS' => _t(__CLASS__.'.permshint', 'View create and edit FAQs in FAQ block'),
+        ];
     }
 
-    public function canView($member = null, $context=[]) {
-      $member = Security::getCurrentUser();
-      return Permission::check('CAN_ACCESS_FAQS', 'any', $member);
+    public function canView($member = null, $context = [])
+    {
+        $member = Security::getCurrentUser();
+        return Permission::check('CAN_ACCESS_FAQS', 'any', $member);
     }
 
-    public function canCreate($member = null, $context=[]) {
-      $member = Security::getCurrentUser();
-      return Permission::check('CAN_ACCESS_FAQS', 'any', $member);
+    public function canCreate($member = null, $context = [])
+    {
+        $member = Security::getCurrentUser();
+        return Permission::check('CAN_ACCESS_FAQS', 'any', $member);
     }
 
-    public function canEdit($member = null, $context=[]) {
-      $member = Security::getCurrentUser();
-      return Permission::check('CAN_ACCESS_FAQS', 'any', $member);
+    public function canEdit($member = null, $context = [])
+    {
+        $member = Security::getCurrentUser();
+        return Permission::check('CAN_ACCESS_FAQS', 'any', $member);
     }
 
     public function getCMSFields()
